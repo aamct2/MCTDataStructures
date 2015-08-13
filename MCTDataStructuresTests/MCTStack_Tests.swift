7cb6895e12d1153e5a2203b4db005ba0236377ef
aamct2 <opensource@Aarons-MacBook-Pro.local>
2015-08-13 17:16:28 -0500

    Finished adding stack tests

--- MCTDataStructuresTests/MCTStack_Tests.swift
+++ MCTDataStructuresTests/MCTStack_Tests.swift
@@ -11,9 +11,15 @@ import XCTest
 
 class MCTStack_Tests: XCTestCase {
     
+    var testStack = MCTStack<Int>!()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        testStack = MCTStack<Int>()
+        testStack.pushObject(-1)
+        testStack.pushObject(3)
     }
     
     override func tearDown() {
@@ -27,6 +33,103 @@ class MCTStack_Tests: XCTestCase {
         let emptyStack = MCTStack<Int>()
         
         XCTAssertNotNil(emptyStack)
+    }
+    
+    // MARK: - Properties
+    
+    func testSize() {
+        testStack.pushObject(4)
+        
+        XCTAssertEqual(testStack.size, 3)
+        
+        testStack.popObject()
+        
+        XCTAssertEqual(testStack.size, 2)
+        
+        testStack.popAll()
+        
+        XCTAssertEqual(testStack.size, 0)
+    }
+    
+    func testIsEmpty() {
+        XCTAssertFalse(testStack.isEmpty)
+        
+        testStack.popAll()
+        
+        XCTAssertTrue(testStack.isEmpty)
+    }
+    
+    func testDescription() {
+        XCTAssertEqual(testStack.description, "::-1::3")
+    }
+    
+    // MARK: - Methods
+    
+    func testPopObject() {
+        XCTAssertEqual(testStack.popObject(), 3)
+        XCTAssertEqual(testStack.popObject(), -1)
+        
+        XCTAssertNil(testStack.popObject())
+    }
+    
+    func testPushObject() {
+        let newStack = MCTStack<Int>()
+        
+        newStack.pushObject(-1)
+        newStack.pushObject(3)
+        
+        XCTAssertEqual(newStack.peek(), 3)
+    }
+    
+    func testPopAll() {
+        testStack.popAll()
+        
+        XCTAssertEqual(testStack.size, 0)
+    }
+    
+    func testPeek() {
+        XCTAssertEqual(testStack.peek(), 3)
+        
+        testStack.popAll()
+        
+        XCTAssertNil(testStack.peek())
+    }
+    
+    func testReverseStack() {
+        XCTAssertEqual(testStack.peek(), 3)
+        
+        testStack.reverseStack()
+        
+        XCTAssertEqual(testStack.peek(), -1)
+    }
+    
+    func testStackAsArray() {
+        let testArray = testStack.stackAsArray()
+        
+        XCTAssertEqual(testArray.count, 2)
+        XCTAssertEqual(testArray[0], -1)
+        XCTAssertEqual(testArray[1], 3)
+    }
+    
+    func testGenerator() {
+        var result = 0
+        
+        for item in testStack {
+            result += item
+        }
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func testCopy() {
+        let newStack = testStack.copy()
+        
+        testStack.pushObject(4)
+        
+        XCTAssertEqual(testStack.size, 3)
+        XCTAssertEqual(newStack.size, 2)
+        XCTAssertEqual(newStack.popObject(), 3)
+        XCTAssertEqual(newStack.popObject(), -1)
     }
     
 }
